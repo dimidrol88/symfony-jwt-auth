9b7ae1ca0378197d0f817c3be98ff86117d6ee21
Dmitriy Kuznetsov <kuznetsov.d.a@effective-group.ru>
2022-11-03 14:37:57 +0300

    Install symfony skeleton

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/api/", name="home")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'status' => 'ok',
+            'data' => [
+                'page' => 'main-page'
+            ]
+        ]);
+    }
+}
